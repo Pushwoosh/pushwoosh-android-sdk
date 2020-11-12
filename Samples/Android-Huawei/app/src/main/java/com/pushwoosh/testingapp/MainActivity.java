2be462215984247bff6311482d10e3f5be948618
Nikolay Galizin <ngalizin@pushwoosh.com>
2020-11-12 21:56:08 +0700

    Added Android-Huawei and Android-Firebase-And-Huawei samples

---
+++ Samples/Android-Huawei/app/src/main/java/com/pushwoosh/testingapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.pushwoosh.testingapp;
+
+import android.os.Bundle;
+
+import com.pushwoosh.Pushwoosh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		Pushwoosh.getInstance().registerForPushNotifications();
+	}
+}
